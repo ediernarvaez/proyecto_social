f720f79c516239e5f30dfb2a6ff2ae0c49b3a611
edierpapamija <eypapamija2@misena.edu.co>
2022-10-08 20:30:07 -0500

    router

--- src/layout/Base.jsx
+++ src/layout/Base.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import NotFoundPage from '../pages/NotFoundPage';
 
 function Base() {
-  return <div>Base</div>;
+  return (
+    <main>
+      <Router>
+        <Routes>
+          {/* home */}
+          <Route path="/" element={<HomePage />} />
+          {/* not found */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </main>
+  );
 }
 
 export default Base;
