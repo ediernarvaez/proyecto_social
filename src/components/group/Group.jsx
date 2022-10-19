98872c00d6a9a7a15fea633441b2539e4a1e9a2f
Manuekle <elustondo129@gmail.com>
2022-10-18 21:35:09 -0500

    group

--- src/components/group/Group.jsx
+++ src/components/group/Group.jsx
@@ -5,11 +5,11 @@ import GroupIcon from '../assets/svg/GroupIcon';
 
 function Group() {
   return (
-    <section className="flex flex-col pt-20 bg-white dark:bg-zinc-800">
+    <section className="flex flex-col pt-20 bg-white dark:bg-zinc-800 px-16 gap-8">
       <h1 className="flex text-zinc-800 dark:text-zinc-100 text-4xl capitalize font-bold justify-start">
         Grupos
       </h1>
-      <div className="grid grid-cols-6 gap-8 px-16 pt-10">
+      <div className="grid grid-cols-6 gap-8">
         <div className="col-span-3 rounded-md bg-yellow-100 dark:bg-zinc-700 bg-dark-200 shadow-lg">
           <div className="grid grid-cols-2">
             <div className="col-span-1 flex justify-start p-8 relative">
@@ -69,7 +69,7 @@ function Group() {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-6 gap-8 px-16 pt-10">
+      <div className="grid grid-cols-6 gap-8">
         <div className="col-span-3 rounded-md bg-yellow-100 dark:bg-zinc-700 bg-dark-200 shadow-lg">
           <div className="grid grid-cols-2">
             <div className="col-span-1 flex justify-start p-8 relative">
@@ -129,34 +129,30 @@ function Group() {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-6 gap-8 px-16 pt-10 ">
-        <div className="col-span-3 rounded-md bg-yellow-100 dark:bg-zinc-700 bg-dark-200 shadow-lg flex justify-center border">
-          <div className="grid grid-cols-2">
-            <div className="col-span-1 flex justify-start p-8 relative">
-              <span className="flex flex-col gap-7">
-                <p className="text-zinc-900 uppercase tracking-widest dark:text-zinc-100 font-bold text-3xl pt-20">
-                  GRUPO QUINTO
-                </p>
-                <span className="flex justify-start">
-                  <Link to="/watchProjets">
-                    <button
-                      type="button"
-                      className="bg-sky-500 rounded-md px-10 py-2"
-                    >
-                      <h1 className="text-zinc-50 font-bold text-xs">
-                        Ver más
-                      </h1>
-                    </button>
-                  </Link>
-                </span>
+      <div className="mx-auto dark:bg-zinc-700 shadow-lg rounded-md p-4">
+        <div className="grid grid-cols-2">
+          <div className="col-span-1 flex justify-start p-8 relative">
+            <span className="flex flex-col gap-7">
+              <p className="text-zinc-900 uppercase tracking-widest dark:text-zinc-100 font-bold text-3xl pt-20">
+                GRUPO QUINTO
+              </p>
+              <span className="flex justify-start">
+                <Link to="/watchProjets">
+                  <button
+                    type="button"
+                    className="bg-sky-500 rounded-md px-10 py-2"
+                  >
+                    <h1 className="text-zinc-50 font-bold text-xs">Ver más</h1>
+                  </button>
+                </Link>
               </span>
-            </div>
-            <div className="col-span-1 flex justify-end">
-              <GroupIcon
-                alt="Libros hasta un 30% off"
-                className="rounded-r-md object-cover mt-5"
-              />
-            </div>
+            </span>
+          </div>
+          <div className="col-span-1 flex justify-end">
+            <GroupIcon
+              alt="Libros hasta un 30% off"
+              className="rounded-r-md object-cover mt-5"
+            />
           </div>
         </div>
       </div>
