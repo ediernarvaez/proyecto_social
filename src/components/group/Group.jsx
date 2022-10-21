866b66ac0564b4ca677a57a8111024d8d611d8d2
edierpapamija <eypapamija2@misena.edu.co>
2022-10-20 23:58:20 -0500

    cambios

--- src/components/group/Group.jsx
+++ src/components/group/Group.jsx
@@ -12,21 +12,23 @@ function Group() {
       <div className="bg-white dark:bg-zinc-900 rounded-lg shadow-md shadow-zinc-900 p-8 w-full lg:px-8 lg:w-[800px]">
         <h2 className="text-3xl font-black text-center text-white sm:text-4xl">
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-amber-300 via-amber-500 to-amber-100">
-            Lista de Grupos
+            LISTA DE GRUPOS
           </span>
         </h2>
         <div className="grid grid-cols-4 grid-rows-3 gap-8 justify-center pt-8">
           <div className="col-span-4 lg:col-span-2 bg-zinc-800/40 rounded-lg">
             <span className="flex flex-col lg:flex-row gap-2 items-start p-4 justify-between">
               <span>
-                <h1 className="text-white font-bold text-xl">Grupo 1</h1>
-                <h1 className="text-normal font-normal text-white text-sm">
+                <h1 className="dark:text-zinc-100 text-black font-bold text-xl">
+                  Grupo 1
+                </h1>
+                <h1 className="text-normal font-normal dark:text-zinc-100 text-black text-sm">
                   Haz click para ver el diseño
                 </h1>
                 <a
                   href="https://www.figma.com/proto/6TjeBmOPtaf0xTbPwcgpRE/proyecto_social?page-id=0%3A1&node-id=1%3A2&viewport=266%2C161%2C0.33&scaling=scale-down"
                   target="_blank"
-                  className="text-normal font-normal text-indigo-400 text-sm hover:underline"
+                  className="text-normal font-normal dark:text-indigo-400 text-blue-800 text-sm hover:underline"
                   rel="noreferrer"
                 >
                   Ver diseño
@@ -45,14 +47,16 @@ function Group() {
           <div className="col-span-4 lg:col-span-2 bg-zinc-800/40 rounded-lg">
             <span className="flex flex-col lg:flex-row gap-2 items-start p-4 justify-between">
               <span>
-                <h1 className="text-white font-bold text-xl">Grupo 1</h1>
-                <h1 className="text-normal font-normal text-white text-sm">
+                <h1 className="dark:text-zinc-100 text-black font-bold text-xl">
+                  Grupo 2
+                </h1>
+                <h1 className="text-normal font-normal dark:text-zinc-100 text-black text-sm">
                   Haz click para ver el diseño
                 </h1>
                 <a
                   href="https://www.figma.com/proto/6TjeBmOPtaf0xTbPwcgpRE/proyecto_social?page-id=0%3A1&node-id=1%3A2&viewport=266%2C161%2C0.33&scaling=scale-down"
                   target="_blank"
-                  className="text-normal font-normal text-indigo-400 text-sm hover:underline"
+                  className="text-normal font-normal dark:text-indigo-400 text-blue-800 text-sm hover:underline"
                   rel="noreferrer"
                 >
                   Ver diseño
@@ -71,14 +75,16 @@ function Group() {
           <div className="col-span-4 lg:col-span-2 bg-zinc-800/40 rounded-lg">
             <span className="flex flex-col lg:flex-row gap-2 items-start p-4 justify-between">
               <span>
-                <h1 className="text-white font-bold text-xl">Grupo 1</h1>
-                <h1 className="text-normal font-normal text-white text-sm">
+                <h1 className="dark:text-zinc-100 text-black font-bold text-xl">
+                  Grupo 3
+                </h1>
+                <h1 className="text-normal font-normal dark:text-zinc-100 text-black text-sm">
                   Haz click para ver el diseño
                 </h1>
                 <a
                   href="https://www.figma.com/proto/6TjeBmOPtaf0xTbPwcgpRE/proyecto_social?page-id=0%3A1&node-id=1%3A2&viewport=266%2C161%2C0.33&scaling=scale-down"
                   target="_blank"
-                  className="text-normal font-normal text-indigo-400 text-sm hover:underline"
+                  className="text-normal font-normal dark:text-indigo-400 text-blue-800 text-sm hover:underline"
                   rel="noreferrer"
                 >
                   Ver diseño
@@ -97,14 +103,16 @@ function Group() {
           <div className="col-span-4 lg:col-span-2 bg-zinc-800/40 rounded-lg">
             <span className="flex flex-col lg:flex-row gap-2 items-start p-4 justify-between">
               <span>
-                <h1 className="text-white font-bold text-xl">Grupo 1</h1>
-                <h1 className="text-normal font-normal text-white text-sm">
+                <h1 className="dark:text-zinc-100 text-black font-bold text-xl">
+                  Grupo 4
+                </h1>
+                <h1 className="text-normal font-normal dark:text-zinc-100 text-black text-sm">
                   Haz click para ver el diseño
                 </h1>
                 <a
                   href="https://www.figma.com/proto/6TjeBmOPtaf0xTbPwcgpRE/proyecto_social?page-id=0%3A1&node-id=1%3A2&viewport=266%2C161%2C0.33&scaling=scale-down"
                   target="_blank"
-                  className="text-normal font-normal text-indigo-400 text-sm hover:underline"
+                  className="text-normal font-normal dark:text-indigo-400 text-blue-800 text-sm hover:underline"
                   rel="noreferrer"
                 >
                   Ver diseño
@@ -124,14 +132,16 @@ function Group() {
           <div className="col-span-4 lg:col-span-2 bg-zinc-800/40 rounded-lg">
             <span className="flex flex-col lg:flex-row gap-2 items-start p-4 justify-between">
               <span>
-                <h1 className="text-white font-bold text-xl">Grupo 1</h1>
-                <h1 className="text-normal font-normal text-white text-sm">
+                <h1 className="dark:text-zinc-100 text-black font-bold text-xl">
+                  Grupo 5
+                </h1>
+                <h1 className="text-normal font-normal dark:text-zinc-100 text-black text-sm">
                   Haz click para ver el diseño
                 </h1>
                 <a
                   href="https://www.figma.com/proto/6TjeBmOPtaf0xTbPwcgpRE/proyecto_social?page-id=0%3A1&node-id=1%3A2&viewport=266%2C161%2C0.33&scaling=scale-down"
                   target="_blank"
-                  className="text-normal font-normal text-indigo-400 text-sm hover:underline"
+                  className="text-normal font-normal dark:text-indigo-400 text-blue-800 text-sm hover:underline"
                   rel="noreferrer"
                 >
                   Ver diseño
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import ReactTooltip from 'react-tooltip';
 import { Link } from 'react-router-dom';
 
 import DarkModeSwitch from '../DarkMode/DarkModeSwitch';
@@ -9,7 +9,7 @@ import Logout from '../assets/svg/logout';
 
 function Navbar() {
   return (
-    <section className="mx-auto px-56 py-4 dark:bg-zinc-900 bg-yellow-100">
+    <section className="mx-auto px-56 py-4 dark:bg-zinc-900 bg-blue-200">
       <div className="container grid grid-cols-3">
         <div className="flex justify-start items-center col-span-1">
           <Link to="/">
@@ -19,10 +19,31 @@ function Navbar() {
         <div className="flex flex-row gap-2 justify-end items-center col-span-2 ">
           <DarkModeSwitch />
           <Link to="/formulario">
-            <Library className="dark:fill-white fill-black" />
+            <Library
+              className="dark:fill-white fill-black"
+              data-tip
+              data-for="form"
+            />
+            <ReactTooltip id="form" place="top" effect="solid">
+              Registrar Proyecto
+            </ReactTooltip>
           </Link>
-          <Logout className="dark:fill-white fill-black" />
-          <Github className="dark:fill-white fill-black" />
+          <Logout
+            className="dark:fill-white fill-black"
+            data-tip
+            data-for="logout"
+          />
+          <ReactTooltip id="logout" place="top" effect="solid">
+            Cerrar Sesión
+          </ReactTooltip>
+          <Github
+            className="dark:fill-white fill-black"
+            data-tip
+            data-for="git"
+          />
+          <ReactTooltip id="git" place="top" effect="solid">
+            Git Hub
+          </ReactTooltip>
         </div>
       </div>
     </section>
